a0ebd496b6109d3c554cd8c4f689daa60ba572b4
Matías <matiasagnolin@gmail.com>
2018-11-30 16:50:48 +0100

    Update FutbolAppSeedJob.groovy

--- Jenkins/seedjobs/FutbolAppSeedJob.groovy
+++ Jenkins/seedjobs/FutbolAppSeedJob.groovy
@@ -61,7 +61,7 @@ def createCiJob(def jobName, def gitUrl, def pomFile) {
           skipNoTestFiles(true)
           stopProcessingIfError(true)
         }
-      }*/
+      }
       publishCloneWorkspace('**', '', 'Any', 'TAR', true, null)
       downstreamParameterized {
         trigger("${jobName}-2-sonar") {
@@ -73,11 +73,11 @@ def createCiJob(def jobName, def gitUrl, def pomFile) {
     }
   }
 }
-/*
+
 def createSonarJob(def jobName, def gitUrl, def pomFile) {
   job("${jobName}-2-sonar") {
     parameters {
-      stringParam("BRANCH", "master", "Define TAG or BRANCH to build from")
+      stringParam("BRANCH", "develop", "Define TAG or BRANCH to build from")
     }
     scm {
       cloneWorkspace("${jobName}-1-ci")
@@ -208,7 +208,7 @@ listView('Futbol App') {
     filterBuildQueue()
     filterExecutors()
     jobs {
-        regex(/conference-app-.*/)
+        regex(/conference-app-.)
   /*  }
     columns {
         status()
